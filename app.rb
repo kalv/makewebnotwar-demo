6b2942798badba567708c91dd3ad07a4a0caf22e
Kalvir Sandhu <kalv@kalv.co.uk>
2012-04-17 09:47:01 -0700

    Adding some comments so that I can better walk through the code

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ get "/" do
   redirect "/index.html"
 end
 
+# Set up a Server-Sent Event connection
 get "/sse_endpoint" do
   content_type 'text/event-stream'
   stream(:keep_open) { |out|
@@ -13,6 +14,8 @@ get "/sse_endpoint" do
   }
 end
 
+# Drawer has created some lines send
+# to connected clients
 post '/drawlines' do
   # write to all open streams
   connections.each {|out|
@@ -21,9 +24,13 @@ post '/drawlines' do
   "message sent"
 end
 
+# Guesser has created an answer send to
+# connected clients
 post '/answers' do
   connections.each {|out|
     out << "event:answer\ndata:#{params[:answer]}\n\n"
   }
   "message sent"
 end
+# this could've been improved by storing different
+# connections for drawers / guessers
